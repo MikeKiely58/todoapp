b7f3154f926374d36d0f52ade29d39d3851daab7
Ubuntu <KielyM@VMtodoapp.kbnilhvgkdduhkrhzefihu4nha.bx.internal.cloudapp.net>
2021-12-14 17:25:43 +0000

    syntax issues

--- Jenkins/test.sh
+++ Jenkins/test.sh
@@ -3,6 +3,6 @@
 source venv/bin/activate
 python3 -m pytest \
     --cov=applicaton \
-    --cov-report= term-missing \
+    --cov-report term-missing \
     --cov-report xml:coverage.xml \
-    --junit-xml=junit_report.xml
+    --junitxml=junit_report.xml
